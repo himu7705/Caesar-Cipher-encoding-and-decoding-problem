eeaf47a8e6f4b76f7ace3c8b79c9fa0b4d2f119f
himu7705 <140712133+himu7705@users.noreply.github.com>
2025-07-26 12:19:29 +0530

    problem-solved

--- caesercipher.py
+++ caesercipher.py
@@ -1,21 +1,16 @@
 def encrypt_caesar(msg, shift):
     res = ''
-    
     for ch in msg:
         if ch.isalpha():
             if ch.isupper():
                 base = ord('A')
             else:
                 base = ord('a')
-            
             code = ord(ch) - base
             code = (code + shift) % 26
             res += chr(base + code)
         else:
             res += ch
-            
     return res
-
-
 def decrypt_caesar(msg, shift):
     return encrypt_caesar(msg, -shift)
